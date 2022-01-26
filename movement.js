451c8ed6cd228ec0a130feaffd006862725e43f8
chuefengv <feng.vang98@gmail.com>
2022-01-25 19:27:22 -0500

    on click, spin

--- movement.js
+++ movement.js
@@ -1,4 +1,9 @@
-anime({
+var spin = anime({
     targets: 'div#character',
-    rotate: '1turn'
+    rotate: '1turn',
+    autoplay: false
 });
+
+var animation = document.getElementById('character');
+
+animation.addEventListener('click', spin.play, false);
